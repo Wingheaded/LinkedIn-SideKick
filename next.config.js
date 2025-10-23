5a7cc2da790f824d7a66a621e6ec120b316503a3
Jose Antonio Gomes <159716865+Wingheaded@users.noreply.github.com>
2025-10-23 19:23:57 +0100

    Fix formatting and export statement in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true },
-  images: { remotePatterns: [
-    { protocol: 'https', hostname: '**' },
-    { protocol: 'http', hostname: '**' },
-  ]},
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http',  hostname: '**' },
+    ],
+  },
 };
+
 export default nextConfig;
